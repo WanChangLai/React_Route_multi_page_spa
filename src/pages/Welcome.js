a59b008d74086dab025753c91cbab627b7deb598
Wan Chang Lai <wanchang.lai@live.com>
2022-11-26 16:52:59 +0800

    Working with nested route and Add Redirect Route

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,16 @@
+import { Route } from "react-router-dom";
+
 const Welcome = () => {
-  return <h1>The Welcome Page!</h1>;
+  return (
+    <section>
+      <h1>The Welcome Page!</h1>
+      <Route path="/welcome/new-user">
+        <p>Welcome, new user!</p>
+      </Route>
+    </section>
+  );
 };
 
 export default Welcome;
+
+//nested route means that route does not have to be in the same js file
