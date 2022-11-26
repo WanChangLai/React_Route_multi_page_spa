94460e6e0d0ed836f88d5156513a3a90fd5d68a8
Wan Chang Lai <wanchang.lai@live.com>
2022-11-26 16:07:24 +0800

    Add useParams and display the parameters

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,7 +1,15 @@
+import { useParams } from "react-router-dom";
+
 const ProductDetail = () => {
+  //param refers to the character after :
+  const params = useParams();
+
+  console.log(params.productId);
+
   return (
     <section>
       <h1>Product Detail</h1>
+      <p>{params.productId}</p>
     </section>
   );
 };
